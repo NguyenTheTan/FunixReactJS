870731ebbabb7f658cf94da663992d07a1245375
NguyenTheTan <tanntfx13442@funix.edu.vn>
2021-12-29 19:16:35 +0700

    commit9

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,16 +7,17 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      staffs: STAFFS  
+      staffs: STAFFS,
     };
   }
+
   render() {
     return (
       <div className="App">
         <div>
           <div className= "Header">
             <h3 className= "heading" >Ứng dụng quản lý nhân sự v1.0</h3>
-          </div>
+          </div>         
         </div>
         <StaffList staff={this.state.staffs} />
       </div>
