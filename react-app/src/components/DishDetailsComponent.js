d4cdd7bd0cfb3e2bf4e3d5b5bbb61399ba73f5cf
NguyenTheTan <tanntfx13442@funix.edu.vn>
2021-12-31 23:41:54 +0700

    1

--- react-app/src/components/DishDetailsComponent.js
+++ react-app/src/components/DishDetailsComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardText, CardTitle, CardImg, CardBody } from 'reactstrap';
+import { Card, CardText, CardTitle, CardImg, CardBody,Breadcrumb,BreadcrumbItem } from 'reactstrap';
 import  dateFormat  from "dateformat";
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
         return(
@@ -44,8 +45,18 @@ const DishDetail = (props) => {
         return(
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div> 
+                </div>               
+                <div className="row">
                     <RenderDish dish= {props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         )
@@ -53,6 +64,8 @@ const DishDetail = (props) => {
         return(
             <div></div>
         )
+    
 }
+
 
 export default DishDetail;
